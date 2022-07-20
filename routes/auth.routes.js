e9231c966b78a7e60518dc6063f4e1a81d6c5c1e
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-07-20 11:04:38 +0100

    sign in route - tested - fully working

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,7 +14,7 @@ const saltRounds = 10;
  */
 
 router.post("/signup", async (req, res, next) => {
-  const {userType} = req.body;
+  const { userType } = req.body;
   if (userType === "buyer") {
     const {
       name,
@@ -27,11 +27,9 @@ router.post("/signup", async (req, res, next) => {
     } = req.body;
     console.log("req.body", req.body);
     if (email === "" || name === "" || password === "" || address === "") {
-      res
-        .status(400)
-        .json({
-          message: "Please provide your email, name, password and address",
-        });
+      res.status(400).json({
+        message: "Please provide your email, name, password and address",
+      });
     }
 
     // ! To use only if you want to enforce strong password (not during dev-time)
@@ -87,70 +85,63 @@ router.post("/signup", async (req, res, next) => {
       return res.status(500).json({ errorMessage: error.message });
     }
   } else {
-		const {
-		  name,
-		  email,
-		  password,
-		  address,
-		} = req.body;
-		console.log("req.body", req.body);
-		if (email === "" || name === "" || password === "" || address === "") {
-		  res
-			.status(400)
-			.json({
-			  message: "Please provide your email, name, password and address",
-			});
-		}
-	
-		// ! To use only if you want to enforce strong password (not during dev-time)
-	
-		// const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-	
-		// if (!regex.test(password)) {
-		// 	return res
-		// 		.status(400)
-		// 		.json({
-		// 			message:
-		// 				"Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter.",
-		// 		});
-		// }
-	
-		try {
-		  const foundOwner = await Owner.findOne({ email });
-		  console.log("foundOwner", foundOwner);
-		  if (foundOwner) {
-			res
-			  .status(400)
-			  .json({ message: "Email already registered, please login." });
-			return;
-		  }
-		  const salt = bcrypt.genSaltSync(saltRounds);
-		  const hashedPass = bcrypt.hashSync(password, salt);
-	
-		  const createdOwner = await Owner.create({
-			name,
-			email,
-			password: hashedPass,
-			address,
-		  });
-	
-		  const user = createdOwner.toObject();
-		  delete user.password;
-		  // ! Sending the user as json to the client
-		  res.status(201).json({ user });
-		} catch (error) {
-		  console.log(error);
-		  if (error instanceof mongoose.Error.ValidationError) {
-			return res.status(400).json({ message: error.message });
-		  }
-		  if (error.code === 11000) {
-			return res.status(400).json({
-			  errorMessage:
-				"Only one can be associated to an email. The email you chose is already in use.",
-			});
-		  }
-		  return res.status(500).json({ errorMessage: error.message });
-		}
+    const { name, email, password, address } = req.body;
+    console.log("req.body", req.body);
+    if (email === "" || name === "" || password === "" || address === "") {
+      res.status(400).json({
+        message: "Please provide your email, name, password and address",
+      });
+    }
+
+    // ! To use only if you want to enforce strong password (not during dev-time)
+
+    // const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+
+    // if (!regex.test(password)) {
+    // 	return res
+    // 		.status(400)
+    // 		.json({
+    // 			message:
+    // 				"Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    // 		});
+    // }
+
+    try {
+      const foundOwner = await Owner.findOne({ email });
+      console.log("foundOwner", foundOwner);
+      if (foundOwner) {
+        res
+          .status(400)
+          .json({ message: "Email already registered, please login." });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPass = bcrypt.hashSync(password, salt);
+
+      const createdOwner = await Owner.create({
+        name,
+        email,
+        password: hashedPass,
+        address,
+      });
+
+      const user = createdOwner.toObject();
+      delete user.password;
+      // ! Sending the user as json to the client
+      res.status(201).json({ user });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({ message: error.message });
+      }
+      if (error.code === 11000) {
+        return res.status(400).json({
+          errorMessage:
+            "Only one can be associated to an email. The email you chose is already in use.",
+        });
+      }
+      return res.status(500).json({ errorMessage: error.message });
+    }
   }
 });
 
@@ -160,14 +151,43 @@ router.post("/signin", async (req, res, next) => {
     res.status(400).json({ message: "Please provide your email and password" });
   }
   try {
-    const foundUser = await User.findOne({ email });
-    if (!foundUser) {
-      res.status(500).json({ message: "Wrong credentials." });
-      return;
+    const foundBuyer = await Buyer.findOne({ email });
+    if (!foundBuyer) {
+      const foundOwner = await Owner.findOne({ email });
+      if (!foundOwner) {
+        res.status(500).json({ message: "Wrong credentials." });
+        return;
+      }
+      const goodPass = bcrypt.compareSync(password, foundOwner.password);
+      if (goodPass) {
+        const user = foundOwner.toObject();
+        delete user.password;
+
+        /**
+         * Sign method allow you to create the token.
+         *
+         * ---
+         *
+         * - First argument: user, should be an object. It is our payload !
+         * - Second argument: A-really-long-random-string...
+         * - Third argument: Options...
+         */
+
+        const authToken = jwt.sign(user, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "2d",
+        });
+
+        //! Sending the authToken to the client !
+
+        res.status(200).json({ authToken });
+      } else {
+        res.status(500).json({ message: "Wrong credentials." });
+      }
     }
-    const goodPass = bcrypt.compareSync(password, foundUser.password);
+    const goodPass = bcrypt.compareSync(password, foundBuyer.password);
     if (goodPass) {
-      const user = foundUser.toObject();
+      const user = foundBuyer.toObject();
       delete user.password;
 
       /**
