489246962883a250e03625c330b52de0778f8133
Lukas Baur <baurlukas89@gmail.com>
2022-07-22 13:05:27 +0200

    routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -14,7 +14,9 @@ router.get("/", isAuthenticated, async (req, res, next) => {
   //returns user data
   try {
     const foundUserId = req.payload._id;
-    const foundUser = await Buyer.findById(foundUserId) || await Owner.findById(foundUserId);
+    const foundUser =
+      (await Buyer.findById(foundUserId)) ||
+      (await Owner.findById(foundUserId));
     // using descriptors: if uncommenting the next line, please comment the line above
     //const foundUser = await MegaUser.findById(foundUserId);
     return res.status(201).json({ foundUser });
@@ -27,25 +29,20 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 router.put("/", isAuthenticated, async (req, res, next) => {
   //allows user to edit own profile
   try {
-    const {
-      name,
-      email,
-      address,
-    } = req.body;
-    if (
-      name === "" ||
-      email === "" ||
-      address === ""
-    ) {
+    const id = req.payload._id;
+    const { name, email, address } = req.body;
+    if (name === "" || email === "" || address === "") {
       res.status(400).json({
         message: "Please make sure you fill all mandatory fields",
       });
     }
-    console.log("req.body editing profile", req.body)
-    const updatedUser = await Buyer.findByIdAndUpdate(req.body, { new: true }) || await Owner.findByIdAndUpdate(req.body, { new: true }); 
+    console.log("req.body editing profile", req.body);
+    const updatedUser =
+      (await Buyer.findByIdAndUpdate(id, req.body, { new: true })) ||
+      (await Owner.findByIdAndUpdate(id, req.body, { new: true }));
     // using descriptors: if uncommenting the next line, please comment the line above
     // const updatedUser = await MegaUser.findByIdAndUpdate(req.body, { new: true });
-    console.log(updatedUser)
+    console.log(updatedUser);
     return res.status(201).json({ updatedUser });
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
