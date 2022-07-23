2d1b019d2c79f7ad8d0c20f8ba248769b2eecd54
Lukas Baur <baurlukas89@gmail.com>
2022-07-23 10:51:59 +0200

    image upload

--- routes/common.routes.js
+++ routes/common.routes.js
@@ -5,6 +5,7 @@ const Owner = require("../models/Owner.model");
 // using descriptors: if uncommenting the next line, please comment the 2 lines above
 // const { Owner, Buyer } = require ("../models/MegaUser.model")
 const Dog = require("../models/Dog.model");
+const fileUploader = require("../config/cloudinary.config");
 
 /*
  * * All the routes are prefixed with `/api/common`
@@ -14,7 +15,7 @@ const Dog = require("../models/Dog.model");
 router.get("/:id", isAuthenticated, async (req, res, next) => {
   //shows single dog details
   try {
-    const dog = await Dog.findById(req.params.id).populate('owner');
+    const dog = await Dog.findById(req.params.id).populate("owner");
     return res.status(201).json({ dog });
   } catch (error) {
     return res.status(500).json({ errorMessage: error.message });
@@ -26,8 +27,8 @@ router.get("/", isAuthenticated, async (req, res, next) => {
   //returns user data
   try {
     const foundUser =
-      await Buyer.findById( req.payload._id ) ||
-      await Owner.findById( req.payload._id );
+      (await Buyer.findById(req.payload._id)) ||
+      (await Owner.findById(req.payload._id));
     // using descriptors: if uncommenting the next line, please comment the line above
     //const foundUser = await MegaUser.findById({ _id: req.payload._id });
     const user = foundUser.toObject();
@@ -39,26 +40,40 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 });
 
 //the following route is tested
-router.put("/", isAuthenticated, async (req, res, next) => {
-  //allows user to edit own profile
-  try {
-    const { name, email, address } = req.body;
-    if (name === "" || email === "" || address === "") {
-      res.status(400).json({
-        message: "Please make sure you fill all mandatory fields",
-      });
+router.put(
+  "/",
+  fileUploader.single("imageUrl"),
+  isAuthenticated,
+  async (req, res, next) => {
+    //allows user to edit own profile
+    try {
+      const { name, email, address } = req.body;
+      if (name === "" || email === "" || address === "") {
+        res.status(400).json({
+          message: "Please make sure you fill all mandatory fields",
+        });
+      }
+      if (req.file) {
+        return res.status(201).json({ fileUrl: req.file.path });
+      }
+      res.status(501).json({ errorMessage: "File didn't upload" });
+
+      const updatedUser =
+        (await Buyer.findByIdAndUpdate(req.payload._id, req.body, {
+          new: true,
+        })) ||
+        (await Owner.findByIdAndUpdate(req.payload._id, req.body, {
+          new: true,
+        }));
+      // using descriptors: if uncommenting the next line, please comment the line above
+      // const updatedUser = await MegaUser.findByIdAndUpdate({ _id: req.payload._id }, req.body, { new: true });
+      const user = updatedUser.toObject();
+      delete user.password;
+      return res.status(201).json({ updatedUser });
+    } catch (error) {
+      res.status(500).json({ errorMessage: error.message });
     }
-    const updatedUser =
-      await Buyer.findByIdAndUpdate( req.payload._id, req.body, { new: true }) ||
-      await Owner.findByIdAndUpdate( req.payload._id, req.body, { new: true });
-    // using descriptors: if uncommenting the next line, please comment the line above
-    // const updatedUser = await MegaUser.findByIdAndUpdate({ _id: req.payload._id }, req.body, { new: true });
-    const user = updatedUser.toObject();
-    delete user.password;
-    return res.status(201).json({ updatedUser });
-  } catch (error) {
-    res.status(500).json({ errorMessage: error.message });
   }
-});
+);
 
 module.exports = router;
