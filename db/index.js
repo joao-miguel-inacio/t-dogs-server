d11523eeb51a8bd9758098755926ea3a1b79414b
Lukas Baur <baurlukas89@gmail.com>
2022-08-19 18:39:10 +0200

    mongoose

--- db/index.js
+++ db/index.js
@@ -8,7 +8,11 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
